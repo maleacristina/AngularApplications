1ef921f97f9720ed65571c2ab23515486ebeb2c4
maleacristina <maleacristinaecaterina@gmail.com>
2018-07-10 13:04:02 +0300

    Favorite component-star

--- hello-word/src/app/courses.component.ts
+++ hello-word/src/app/courses.component.ts
@@ -1,15 +1,16 @@
 
 import { Component } from '@angular/core';
+import { PLATFORM_SERVER_ID } from '@angular/common/src/platform_id';
 
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'courses',
     template: `
-       {{ text | summary }}
+       {{ text | summary:10 }}
     `
 })
 
 export class CoursesComponent {
-   text = ' lorem hcgashasha sdaskugash gdasugdakusgd.  dasgdagsduasg dsadgasgd ';
- }
+   text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aliquam ornare wisi eu metus. Integer in sapien.';
+}
--- hello-word/src/app/summary.pipe.ts
+++ hello-word/src/app/summary.pipe.ts
@@ -6,10 +6,11 @@ import { Pipe , PipeTransform } from '@angular/core';
 })
 
 export class SummaryPipe implements PipeTransform {
-    transform(value: any, args?: any) {
+    transform(value: any, limit?: any) {
         if (!value) {
             return null;
         }
-        return value.substr(0, 50) + '...';
+        const actualLimit = (limit) ? limit : 50;
+        return value.substr(0, actualLimit) + '...';
     }
 }
