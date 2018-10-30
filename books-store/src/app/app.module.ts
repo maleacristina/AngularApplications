fc776a9b50ca815ad5a38763b66a30b804e3d7a5
maleacristina <maleacristinaecaterina@gmail.com>
2018-10-30 20:02:36 +0200

    Add project

---
+++ books-store/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { BookComponent } from './components/book/book.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// Forms
+import { FormsModule } from '@angular/forms';
+
+// Material design modules
+import { MatButtonModule } from '@angular/material';
+// Routes
+import { Routes, RouterModule } from '@angular/router';
+const appRoutes: Routes = [
+   {path: '', component: HomeComponent},
+   {path: 'books', component: BooksComponent},
+   {path: 'book/:id', component: BookComponent},
+   {path: 'add-book', component: AddBookComponent},
+   {path: 'edit-book/:id', component: EditBookComponent},
+   {path: 'delete-book/:id', component: DeleteBookComponent}
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BookComponent,
+    AddBookComponent,
+    EditBookComponent,
+    DeleteBookComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
