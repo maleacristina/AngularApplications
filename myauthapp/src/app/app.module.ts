f202044bc3d392a74e5600716fd5233e1ec95b6a
maleacristina <maleacristinaecaterina@gmail.com>
2018-11-22 10:38:11 +0200

    Mail authentication with firebase

---
+++ myauthapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    FormsModule
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ myauthapp/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD7T58Oa4rWcVmwyEIGAtvPvLzZ-VhLkHE',
+    authDomain: 'authenticationapp-d0feb.firebaseapp.com',
+    databaseURL: 'https://authenticationapp-d0feb.firebaseio.com',
+    projectId: 'authenticationapp-d0feb',
+    storageBucket: 'authenticationapp-d0feb.appspot.com',
+    messagingSenderId: '712532776712'
+  }
+};
+
